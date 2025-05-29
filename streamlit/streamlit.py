b8f143d06466c593240c75db811b9770af947f6b
ayichimboy <53350925+ayichimboy@users.noreply.github.com>
2025-05-29 13:02:16 -0500

    changed get_radius

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -180,7 +180,7 @@ latest_log = pdk.Layer(
 view_state = pdk.ViewState(
     latitude=df_latest.iloc[0]['latitude'],
     longitude=df_latest.iloc[0]['longitude'],
-    zoom=15,
+    zoom=10,
     pitch=0
 )
 
