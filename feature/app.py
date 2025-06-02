f332a50b936cc1070d0302fda943d16bfc48fd43
ayichimboy <53350925+ayichimboy@users.noreply.github.com>
2025-06-01 22:22:16 -0500

    changeged file path and added an error notice

--- feature/app.py
+++ feature/app.py
@@ -10,6 +10,7 @@ import pandas as pd
 import json
 import pydeck as pdk
 import cv2
+import os
 
 
 # --- Google Sheets Auth ---
@@ -49,7 +50,12 @@ def extract_gps_from_image(image):
         return None, None
 
 # Run inference with the YOLOv8n model on the 'poop.jpg' image
-new_poop_model = YOLO("final_poop_model.pt")
+model_path = "feature/final_poop_model.pt"
+
+if os.path.exists(model_path):
+    new_poop_model = YOLO(model_path)
+else:
+    st.error(f"Model file not found at: {model_path}")
 
 # Configure Streamlit app
 st.set_page_config(layout='centered', page_title="STL Poopy Tracker", page_icon="🐕💩")
